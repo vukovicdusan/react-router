5ec0b6a03aa0415f76f5a4b5a5b5161395631d76
vukovicdusan <dusan.v.vukovic@gmail.com>
2022-02-23 09:44:17 +0100

    Zone identifiers deleted, nested route added, redirection added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch,  Redirect} from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import MainHeader from './components/MainHeader';
@@ -14,6 +14,9 @@ function App() {
 		<MainHeader />
 		<main>
 	  <Switch>
+	  	<Route path="/" exact >
+		  <Redirect to='/welcome'/>
+		</Route>
 		<Route path="/welcome" >
 			<Welcome></Welcome>
 		</Route>
